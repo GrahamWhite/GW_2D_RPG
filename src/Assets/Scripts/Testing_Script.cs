6de86c44cc03c8ccb099cbd4cc32559043d12d47
Graham White <grahamwhite365@gmail.com>
2021-03-03 18:00:29 -0500

    grid system

---
+++ src/Assets/Scripts/Testing_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing_Script : MonoBehaviour
+{
+
+    private World_Grid grid;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        grid = new World_Grid(4, 4, 10f, new Vector3(20, 0));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+    }
+
+}
